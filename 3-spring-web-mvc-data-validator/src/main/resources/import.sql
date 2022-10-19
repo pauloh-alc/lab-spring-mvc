587fdbbe59d1ae5c3cf64656e627736e9ec2a2cb
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-10-19 19:21:32 -0300

    Adicionando novo projeto
    Esse projeto é uma atualização do projeto Cadastro de Jedi.
    Possui mais validações, uso de uma base de dados em memória enovos
    mapeamentos para novas requisições.

---
+++ 3-spring-web-mvc-data-validator/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into jedi (jedi_id, jedi_name, jedi_lastname) values (111, 'Paulo Henrique', 'Alencar')
+insert into jedi (jedi_id, jedi_name, jedi_lastname) values (200, 'Luara Diniz', 'Alencar')
